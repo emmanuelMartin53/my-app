5d494eec72fb4355e04bd2b244777a6e5093f334
Emmanuel Martin <em.martin456@gmail.com>
2026-01-14 10:49:51 +0100

    update tests getByRole

--- src/components/animals/Animal.test.jsx
+++ src/components/animals/Animal.test.jsx
@@ -5,46 +5,30 @@ import { describe, expect } from 'vitest'
 
 describe('GetByRole', () => {
 
-  test('Affiche le texte Adopter un animal', () => {
+  test('Elements rendus correctement', () => {
     render(<Animal />)
+
     const headingElement = screen.getByRole("heading");
     expect(headingElement).toBeInTheDocument()
-  })
 
-  test('Article rendu correctement', () => {
-    render(<Animal />)
     const articleElement = screen.getByRole('article');
     expect(articleElement).toBeInTheDocument();
 
-  })
-
-  test('Image rendue correctement', () => {
-    render(<Animal />)
     const imageElement = screen.getByRole('img');
     expect(imageElement).toBeInTheDocument();
-  })
 
-  test('Input rendu correctement', () => {
-    render(<Animal />)
     const inputElement = screen.getByRole('textbox');
     expect(inputElement).toBeInTheDocument()
-  })
 
-   test('Select rendu correctement', () => {
-    render(<Animal />)
     const selectElement = screen.getByRole('combobox');
     expect(selectElement).toBeInTheDocument()
-  })
 
-  test('Checkbox rendu correctement', () => {
-    render(<Animal />)
     const checkboxElement = screen.getByRole('checkbox');
     expect(checkboxElement).toBeInTheDocument();
-  })
 
-  test('Button rendu correctement',  () => {
-    render(<Animal />)
     const buttonElement = screen.getByRole('button');
     expect(buttonElement).toBeInTheDocument()
+
   })
+
 })
